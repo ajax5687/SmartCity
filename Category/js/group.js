0d2dd3f5b55b80c40e5bf21d1f3c4998bb22d929
ajai <ajax5687@gmail.com>
2017-09-12 17:00:09 +0530

    smart city upload

---
+++ Category/js/group.js
@@ -0,0 +1,134 @@
+(function($) {
+  'use strict';
+
+
+var margin = {top: -50, right: 20, bottom: 100, left: 70},
+    width = 1024 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var y0 = d3.scale.ordinal()
+    .rangeRoundBands([height, 0], 0.2);
+
+var y1 = d3.scale.linear();
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], 0.1, 0);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+    //.tickFormat(formatDate);
+
+var nest = d3.nest()
+    .key(function(d) { return d.category; });
+
+var stack = d3.layout.stack()
+    .values(function(d) { return d.values; })
+    .x(function(d) { return d.city; })
+    .y(function(d) { return d.cost; })
+    .out(function(d, y0) { d.valueOffset = y0; });
+
+//var color = d3.scale.category10();
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data/data.tsv", function(error, data) {
+
+  data.forEach(function(d) {
+     d.cost = +d.cost;
+  });
+
+  var dataByGroup = nest.entries(data);
+
+  stack(dataByGroup);
+  x.domain(dataByGroup[0].values.map(function(d) { return d.city; }));
+  y0.domain(dataByGroup.map(function(d) { return d.key; }));
+  y1.domain([0, d3.max(data, function(d) { return d.cost; })]).range([y0.rangeBand(), 0]);
+
+  var group = svg.selectAll(".group")
+      .data(dataByGroup)
+    .enter().append("g")
+      .attr("class", "group")
+      .attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
+
+   group.append("text")
+      .attr("class", "label")
+      .attr("x", -6)
+      .attr("y", function(d) { return y1(d.values[0].cost / 2); })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.key; })
+      .style({'opacity': '1'});
+
+  group.selectAll("rect")
+      .data(function(d) { return d.values; })
+    .enter().append("rect")
+      //.style("fill", function(d) { return color(d.category); })
+      .attr("x", function(d) { return x(d.city); })
+      .attr("y", function(d) { return y1(d.cost); })
+      .attr("width", x.rangeBand())
+      .attr("class", function(d){ return "bar " + convertToSlug(d.category)})
+      .attr("height", function(d) { return y0.rangeBand() - y1(d.cost); });
+
+  group.filter(function(d, i) { return !i; }).append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + y0.rangeBand() + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("y", 0)
+      .attr("x", -55)
+      .attr("dy", ".35em")
+      .attr("transform", "rotate(-90)");
+
+  d3.selectAll("input").on("change", change);
+
+  var timeout = setTimeout(function() {
+    d3.select("input[value=\"stacked\"]").property("checked", true).each(change);}, 2000);
+
+  function change() {
+    clearTimeout(timeout);
+    if (this.value === "multiples") transitionMultiples();
+    else transitionStacked();
+  }
+
+  function transitionMultiples() {
+    var t = svg.transition().duration(750), //.ease("bounce").duration(1500)
+        g = t.selectAll(".group").attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
+    g.selectAll("rect").attr("y", function(d) { return y1(d.cost); });
+    g.select(".label")//.attr("y", function(d) { return y1(d.values[0].cost / 2); })
+                      .style({'opacity': '0.8'});
+     hideAnnotation();
+  }
+
+  function transitionStacked() {
+    var t = svg.transition().duration(750),
+        g = t.selectAll(".group").attr("transform", "translate(0," + y0(y0.domain()[0]) + ")");
+    g.selectAll("rect").attr("y", function(d) { return y1(d.cost + d.valueOffset); });
+    g.select(".label")//.attr("y", function(d) { return y1(d.values[0].cost / 2 + d.values[0].valueOffset); })
+                      .style({'opacity': '0'});
+     showAnnotation();
+  }
+});
+
+
+var showAnnotation = function() {
+  $('#annotation').fadeIn(1000);
+};
+
+var hideAnnotation = function () {
+  $('#annotation').fadeOut(200);
+};
+
+function convertToSlug(Text)
+{
+    return Text
+        .toLowerCase()
+        .replace(/[^\w ]+/g,'')
+        .replace(/ +/g,'-')
+        ;
+}
+
+})(jQuery);
